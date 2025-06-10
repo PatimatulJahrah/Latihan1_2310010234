4da7474478d8e2f5a8125f540c606e2af9d0642f
PatimatulJahrah <Fatzah052005@gmail.com>
2025-06-10 14:33:01 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -2,7 +2,16 @@
 package hitung;
 
 public class MatematikaBeraksi {
-  
+    public static void main(String[] args) {
+        // Membuat objek 
+        Matematika patjah = new Matematika(3, 4);
+        
+        System.out.println("Hasil Penjumlahan: " +patjah.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: " +patjah.setPengurangan());
+        System.out.println("Hasil Perkalian: " +patjah.setPerkalian());
+        System.out.println("Hasil Pembagian: " +patjah.setPembagian());
+        
+    }
         
  }
 
